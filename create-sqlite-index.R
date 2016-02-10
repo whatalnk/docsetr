b9dd013ceaa373002176261d0396f6fb4cf66b76
Wataru NAKAI <watnk0806@gmail.com>
2016-02-10 18:37:10 +0900

    Create SQLite index for a package

--- create-sqlite-index.R
+++ create-sqlite-index.R
@@ -1,6 +1,13 @@
-create.sqlite.index <- function(pkg, conn) {
-  dbBegin(conn)
-  doc <- htmlTreeParse(file.path("library", pkg, "html", "00Index.html"), useInternal = TRUE)
+#' Create sqlite index for a package
+#' 
+#' @param docsetroot path to `Documents` dir of docset
+#' @param pkg package name
+#' @param con SQLiteConnection
+#' 
+#' @export
+create.sqlite.index <- function(docsetroot, pkg, con) {
+  dbBegin(con)
+  doc <- htmlTreeParse(file.path(docsetroot, "library", pkg, "html", "00Index.html"), useInternal = TRUE)
   nodes.tr <- getNodeSet(doc, "//table/tr")
   for (n in nodes.tr) {
     nodes.tr.a <- xmlChildren(n)$td %>>% getNodeSet("./a")
@@ -13,8 +20,8 @@ create.sqlite.index <- function(pkg, conn) {
       type <- "Function"
     }
     name <- sub(pattern = "'", replacement = "''", x = name)
-    dbGetQuery(conn, sprintf("insert into searchIndex(name, type, path, package, version) values('%s', '%s', '%s', '%s', '%s')", name, type, path, pkg, packageVersion(pkg)))
+    dbGetQuery(con, sprintf("insert into searchIndex(name, type, path, package, version) values('%s', '%s', '%s', '%s', '%s')", name, type, path, pkg, packageVersion(pkg)))
   }
-  dbGetQuery(conn, sprintf("insert into searchIndex(name, type, path, package, version) values('%s', 'Package', '%s', '%s', '%s')", pkg, file.path("library", pkg, "html", "00Index.html"), pkg , packageVersion(pkg)))
-  dbCommit(conn)
+  dbGetQuery(con, sprintf("insert into searchIndex(name, type, path, package, version) values('%s', 'Package', '%s', '%s', '%s')", pkg, file.path("library", pkg, "html", "00Index.html"), pkg , packageVersion(pkg)))
+  dbCommit(con)
 }
