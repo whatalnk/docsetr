8c34607c30013ec884e7e58bbd845457fdd12350
Wataru NAKAI <watnk0806@gmail.com>
2016-02-10 18:30:58 +0900

    Generate help html for a package

--- generate-help-html.R
+++ generate-help-html.R
@@ -1,3 +1,8 @@
+#' Generate help html files for a package
+#' 
+#' Replace path to R logo, adjust size
+#' 
+#' @param doc 
 replace.logo <- function(doc) {
   oldNode <- newNode <- querySelector(doc, ".toplogo")
   xmlAttrs(newNode)["src"] <- "../../../doc/html/logo.png"
@@ -5,10 +10,16 @@ replace.logo <- function(doc) {
   replaceNodes(oldNode, newNode)
 }
 
+#' Remove navigation
+#' 
+#' @param doc
 remove.navigation <- function(doc) {
   removeNodes(xmlParent(xmlParent(querySelector(doc, ".arrow"))))
 }
 
+#' Replace link
+#' 
+#' @param doc
 replace.link.to.description <- function(doc) {
   nodes <- getNodeSet(doc, "/html/body/ul/li/a")
   nodes %>>% lapply(function(x){
@@ -18,8 +29,13 @@ replace.link.to.description <- function(doc) {
   })
 }
 
-list.of.dir <- function(pkg, path) {
-  viganettes <- list.files(file.path("library", pkg, "doc"))
+#' List of files of doc dir (used when index.html is not exist)
+#' 
+#' @param docsetroot path to `Documents` dir
+#' @param pkg packagename
+#' @param path path to pkg/doc/index.html
+list.of.dir <- function(docsetroot, pkg, path) {
+  viganettes <- list.files(file.path(docsetroot, "library", pkg, "doc"))
   write("<html><head></head><body>", path)
   write(sprintf("<h1>Listing of directory %s</h1><hr><dl>", file.path("library", pkg, "doc")), path, append = TRUE)
   for (l in viganettes) {
@@ -28,33 +44,40 @@ list.of.dir <- function(pkg, path) {
   write("</dl></body></html>", path, append = TRUE)
 }
 
-generate.help.from.Rd <- function(pkg) {
+#' Generate help html from Rd for each topic
+#' 
+#' @docsetroot path to `Documents` dir
+#' @param pkg package name
+generate.help.from.Rd <- function(docsetroot, pkg) {
   pkgRdDB <- tools:::fetchRdDB(file.path(find.package(pkg), 'help', pkg))
   topics <- names(pkgRdDB)
   as.list(topics) %>>% lapply(function(x){
-    tools::Rd2HTML(pkgRdDB[[x]], out = file.path("library", pkg, "html", paste(x, ".html", sep = "")), package = pkg, Links = tools::findHTMLlinks())
+    tools::Rd2HTML(pkgRdDB[[x]], out = file.path(docsetroot, "library", pkg, "html", paste(x, ".html", sep = "")), package = pkg, Links = tools::findHTMLlinks())
   })
 }
 
-
-generate.help.html <- function(pkg) {
-  dir.create(file.path("library", pkg, "html"), recursive = TRUE)
+#' Generate help html for a package
+#' @param docsetroot path to `Documents` dir
+#' @param pkg package name
+#' @export
+generate.help.html <- function(docsetroot, pkg) {
+  dir.create(file.path(docsetroot, "library", pkg, "html"), recursive = TRUE)
   
   # R.css of knitr package
-  file.copy(system.file('misc', 'R.css', package = 'knitr'), file.path("library", pkg, "html"))
+  file.copy(system.file('misc', 'R.css', package = 'knitr'), file.path(docsetroot, "library", pkg, "html"))
   
   # 00index.html
-  file.copy(file.path(find.package(pkg), 'html', "00Index.html"), file.path("library", pkg, "html"))
-  doc <- htmlTreeParse(file.path("library", pkg, "html", "00Index.html"), useInternal = TRUE)
+  file.copy(file.path(find.package(pkg), 'html', "00Index.html"), file.path(docsetroot, "library", pkg, "html"))
+  doc <- htmlTreeParse(file.path(docsetroot, "library", pkg, "html", "00Index.html"), useInternal = TRUE)
   replace.logo(doc)
   remove.navigation(doc)
   replace.link.to.description(doc)
-  saveXML(doc, file.path("library", pkg, "html", "00Index.html"))
+  saveXML(doc, file.path(docsetroot, "library", pkg, "html", "00Index.html"))
   
   # DESCRIPTION, NEWS
   c("DESCRIPTION", "NEWS") %>>% lapply(function(x){
     if (file.exists(system.file(x, package = pkg))) {
-      file.copy(system.file(x, package = pkg), f <- file.path("library", pkg, x), copy.date = TRUE)
+      file.copy(system.file(x, package = pkg), f <- file.path(docsetroot, "library", pkg, x), copy.date = TRUE)
       writeLines(c("<pre>", readLines(f), "</pre>"), paste0(f, ".html"))
       file.remove(f)
     }
@@ -62,10 +85,10 @@ generate.help.html <- function(pkg) {
   
   # doc dir
   if (system.file("doc", package = pkg) != "") {
-    file.copy(system.file("doc", package = pkg), file.path("library", pkg), recursive = TRUE, copy.date = TRUE)
+    file.copy(system.file("doc", package = pkg), file.path(docsetroot, "library", pkg), recursive = TRUE, copy.date = TRUE)
     ## index.html
-    if (file.exists(file.path("library", pkg, "doc", "index.html"))) {
-      doc <- htmlTreeParse(file.path("library", pkg, "doc", "index.html"), useInternal = TRUE)
+    if (file.exists(file.path(docsetroot, "library", pkg, "doc", "index.html"))) {
+      doc <- htmlTreeParse(file.path(docsetroot, "library", pkg, "doc", "index.html"), useInternal = TRUE)
       replace.logo(doc)
       remove.navigation(doc)
       getNodeSet(doc, "/html/head/link") %>>% lapply(function(x){
@@ -73,11 +96,11 @@ generate.help.html <- function(pkg) {
         xmlAttrs(newNode)["href"] <- "../../../doc/html/R.css"
         replaceNodes(oldNode, newNode)
       })
-      saveXML(doc, file.path("library", pkg, "doc", "index.html"))
+      saveXML(doc, file.path(docsetroot, "library", pkg, "doc", "index.html"))
     } else {
-      list.of.dir(pkg, file.path("library", pkg, "doc", "index.html"))
+      list.of.dir(docsetroot, pkg, file.path(docsetroot, "library", pkg, "doc", "index.html"))
     }
   }
   # Help pages
-  generate.help.from.Rd(pkg)
+  generate.help.from.Rd(docsetroot, pkg)
 }
