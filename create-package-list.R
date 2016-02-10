5ae5d629b2531081b01d6dd7d0ab2cfce7ffc616
Wataru NAKAI <watnk0806@gmail.com>
2016-02-10 18:31:47 +0900

    Create list of installed packages

--- create-package-list.R
+++ create-package-list.R
@@ -1,15 +1,19 @@
-# Package list
-create.package.list <- function(){
+#' Create package list
+#'
+#' @param overwrite wherether overwrite or not
+#'
+create.package.list <- function(docsetroot, overwrite = FALSE){
   make.packages.html(temp = TRUE)
-  file.copy(from = file.path(tempdir(), ".R/doc/html/packages.html"), to = file.path("doc", "html"), overwrite = TRUE, copy.date = TRUE)
-  
-  doc <- htmlTreeParse(file.path("doc", "html", "packages.html"), useInternal = TRUE)
-  
+  file.copy(from = file.path(tempdir(), ".R/doc/html/packages.html"), to = file.path(docsetroot, "doc", "html"), overwrite = overwrite, copy.date = TRUE)
+
+  doc <- htmlTreeParse(file.path(docsetroot, "doc", "html", "packages.html"), useInternal = TRUE)
+
   oldNode <- newNode <- querySelector(doc, ".toplogo")
+
   xmlAttrs(newNode)["src"] <- "logo.png"
   addAttributes(newNode, width = "100", height = "78")
   replaceNodes(oldNode, newNode)
-  
+
   remove.navigation(doc)
-  saveXML(doc, file.path("doc", "html", "packages.html"))
+  saveXML(doc, file.path(docsetroot, "doc", "html", "packages.html"))
 }
