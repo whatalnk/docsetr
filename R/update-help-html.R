211033b2c8ec8161c739eccd0b63ef5929068d36
Wataru NAKAI <watnk0806@gmail.com>
2016-02-11 18:12:15 +0900

    Update help html of a package

--- R/update-help-html.R
+++ R/update-help-html.R
@@ -1,47 +1,53 @@
-update.help.html <- function(pkg) {
+#' Update help html
+#'
+#' @param docsetroot path to `Documents` dir
+#' @param pkg package name
+#'
+#' @export
+update.help.html <- function(docsetroot, pkg) {
   # cleanup
   ## help html files
-  htmldir <- file.path("library", pkg, "html")
+  htmldir <- file.path(docsetroot, "library", pkg, "html")
   htmllist <- list.files(htmldir, "*.html")
   file.remove(file.path(htmldir, htmllist))
-  
+
   ## DESCRIPTION, NEWS
   c("DESCRIPTION", "NEWS") %>>% lapply(function(x){
-    if (file.exists(file.path("library", pkg, x))) {
-      file.remove(file.path("library", pkg, x))
+    if (file.exists(file.path(docsetroot, "library", pkg, x))) {
+      file.remove(file.path(docsetroot, "library", pkg, x))
     }
   })
 
   ## doc files
-  if (file.exists(file.path("library", "dplyr", "doc"))) {
-    docdir <- file.path("library", "dplyr", "doc")
+  if (file.exists(file.path(docsetroot, "library", pkg, "doc"))) {
+    docdir <- file.path(docsetroot, "library", pkg, "doc")
     docfiles <- list.files(docdir)
     file.remove(file.path(docdir, docfiles))
   }
-  
+
   # 00index.html
-  file.copy(file.path(find.package(pkg), 'html', "00Index.html"), file.path("library", pkg, "html"))
-  doc <- htmlTreeParse(file.path("library", pkg, "html", "00Index.html"), useInternal = TRUE)
+  file.copy(file.path(find.package(pkg), 'html', "00Index.html"), file.path(docsetroot, "library", pkg, "html"))
+  doc <- htmlTreeParse(file.path(docsetroot, "library", pkg, "html", "00Index.html"), useInternal = TRUE)
   replace.logo(doc)
   remove.navigation(doc)
   replace.link.to.description(doc)
-  saveXML(doc, file.path("library", pkg, "html", "00Index.html"))
-  
+  saveXML(doc, file.path(docsetroot, "library", pkg, "html", "00Index.html"))
+
   # DESCRIPTION, NEWS
   c("DESCRIPTION", "NEWS") %>>% lapply(function(x){
     if (file.exists(system.file(x, package = pkg))) {
-      file.copy(system.file(x, package = pkg), f <- file.path("library", pkg, x), copy.date = TRUE)
+      file.copy(system.file(x, package = pkg), f <- file.path(docsetroot, "library", pkg, x), copy.date = TRUE)
       writeLines(c("<pre>", readLines(f), "</pre>"), paste0(f, ".html"))
       file.remove(f)
     }
   })
-  
+
   # doc dir
   if (system.file("doc", package = pkg) != "") {
-    file.copy(system.file("doc", package = pkg), file.path("library", pkg), recursive = TRUE, copy.date = TRUE)
+    file.copy(system.file("doc", package = pkg), file.path(docsetroot, "library", pkg), recursive = TRUE, copy.date = TRUE)
     ## index.html
-    if (file.exists(file.path("library", pkg, "doc", "index.html"))) {
-      doc <- htmlTreeParse(file.path("library", pkg, "doc", "index.html"), useInternal = TRUE)
+    if (file.exists(file.path(docsetroot, "library", pkg, "doc", "index.html"))) {
+      doc <- htmlTreeParse(file.path(docsetroot, "library", pkg, "doc", "index.html"), useInternal = TRUE)
       replace.logo(doc)
       remove.navigation(doc)
       getNodeSet(doc, "/html/head/link") %>>% lapply(function(x){
@@ -49,11 +55,11 @@ update.help.html <- function(pkg) {
         xmlAttrs(newNode)["href"] <- "../../../doc/html/R.css"
         replaceNodes(oldNode, newNode)
       })
-      saveXML(doc, file.path("library", pkg, "doc", "index.html"))
+      saveXML(doc, file.path(docsetroot, "library", pkg, "doc", "index.html"))
     } else {
-      list.of.dir(pkg, file.path("library", pkg, "doc", "index.html"))
+      list.of.dir(docsetroot, pkg, file.path(docsetroot, "library", pkg, "doc", "index.html"))
     }
   }
   # Help pages
-  generate.help.from.Rd(pkg)
+  generate.help.from.Rd(docsetroot, pkg)
 }
